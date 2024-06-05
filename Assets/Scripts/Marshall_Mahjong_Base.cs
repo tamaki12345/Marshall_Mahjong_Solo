35ed3c4af24537f1a3e98688022fc3b67e019f39
tamaki12345 <tamakihaurta95@gmail.com>
2024-06-06 00:02:24 +0900

    Marshall_Mahojong_Test.cs で配牌と第1ツモを表示するまでを実装s

--- Assets/Scripts/Marshall_Mahjong_Base.cs
+++ Assets/Scripts/Marshall_Mahjong_Base.cs
@@ -14,7 +14,8 @@ namespace Marshall_Mahjong
             }
             else
             {
-                string type, literal = "";
+                string type = "";
+                string literal = "";
 
                 if ( id < 45 )
                 {
@@ -31,85 +32,213 @@ namespace Marshall_Mahjong
 
                 int ltr_id = id % 45;
 
-                if ( ltr_id >= 5 )
+                if ( ltr_id < 5 )
+                {
+                    if ( ltr_id % 5 == 0 )
+                    {
+                        literal = "A";
+                    }
+                    else if ( ltr_id % 5 == 1 )
+                    {
+                        literal = "I";
+                    }
+                    else if ( ltr_id % 5 == 2 )
+                    {
+                        literal = "U";
+                    }
+                    else if ( ltr_id % 5 == 3 )
+                    {
+                        literal = "E";
+                    }
+                    else if ( ltr_id % 5 == 4 )
+                    {
+                        literal = "O";
+                    }
+                }
+                else
                 {
                     if ( ltr_id < 10 )
                     {
-                        literal += "k";
-                    }
-                    else if ( ltr_id < 15 )
-                    {
-                        literal += "s";
-                    }
-                    else if ( ltr_id < 20)
-                    {
-                        literal += "t";
-                    }
-                    else if ( ltr_id < 25 )
-                    {
-                        literal += "n";
-                    }
-                    else if ( ltr_id < 30 )
-                    {
-                        literal += "h";
-                    }
-                    else if ( ltr_id < 35 )
-                    {
-                        literal += "m";
-                    }
-                    else if ( ltr_id < 40 )
-                    {
-                        literal += "r";
-                    }
-                    else if (ltr_id < 43 )
-                    {
-                        literal += "y";
-                    }
-                    else
-                    {
-                        literal += "w";
-                    }
-
-                    if ( ltr_id < 40 )
-                    {
                         if ( ltr_id % 5 == 0 )
                         {
-                            literal += "a";
+                            literal = "KA";
                         }
                         else if ( ltr_id % 5 == 1 )
                         {
-                            literal += "i";
+                            literal = "KI";
                         }
                         else if ( ltr_id % 5 == 2 )
                         {
-                            literal += "u";
+                            literal = "KU";
                         }
                         else if ( ltr_id % 5 == 3 )
                         {
-                            literal += "e";
+                            literal = "KE";
                         }
                         else if ( ltr_id % 5 == 4 )
                         {
-                            literal += "o";
+                            literal = "KO";
+                        }
+                    }
+                    else if ( ltr_id < 15 )
+                    {
+                        if ( ltr_id % 5 == 0 )
+                        {
+                            literal = "SA";
+                        }
+                        else if ( ltr_id % 5 == 1 )
+                        {
+                            literal = "SI";
+                        }
+                        else if ( ltr_id % 5 == 2 )
+                        {
+                            literal = "SU";
+                        }
+                        else if ( ltr_id % 5 == 3 )
+                        {
+                            literal = "SE";
+                        }
+                        else if ( ltr_id % 5 == 4 )
+                        {
+                            literal = "SO";
+                        }
+                    }
+                    else if ( ltr_id < 20)
+                    {
+                        if ( ltr_id % 5 == 0 )
+                        {
+                            literal = "TA";
+                        }
+                        else if ( ltr_id % 5 == 1 )
+                        {
+                            literal = "TI";
+                        }
+                        else if ( ltr_id % 5 == 2 )
+                        {
+                            literal = "TU";
+                        }
+                        else if ( ltr_id % 5 == 3 )
+                        {
+                            literal = "TE";
+                        }
+                        else if ( ltr_id % 5 == 4 )
+                        {
+                            literal = "TO";
+                        }
+                    }
+                    else if ( ltr_id < 25 )
+                    {
+                        if ( ltr_id % 5 == 0 )
+                        {
+                            literal = "NA";
+                        }
+                        else if ( ltr_id % 5 == 1 )
+                        {
+                            literal = "NI";
+                        }
+                        else if ( ltr_id % 5 == 2 )
+                        {
+                            literal = "NU";
+                        }
+                        else if ( ltr_id % 5 == 3 )
+                        {
+                            literal = "NE";
+                        }
+                        else if ( ltr_id % 5 == 4 )
+                        {
+                            literal = "NO";
+                        }
+                    }
+                    else if ( ltr_id < 30 )
+                    {
+                        if ( ltr_id % 5 == 0 )
+                        {
+                            literal = "HA";
+                        }
+                        else if ( ltr_id % 5 == 1 )
+                        {
+                            literal = "HI";
+                        }
+                        else if ( ltr_id % 5 == 2 )
+                        {
+                            literal = "HU";
+                        }
+                        else if ( ltr_id % 5 == 3 )
+                        {
+                            literal = "HE";
+                        }
+                        else if ( ltr_id % 5 == 4 )
+                        {
+                            literal = "HO";
+                        }
+                    }
+                    else if ( ltr_id < 35 )
+                    {
+                        if ( ltr_id % 5 == 0 )
+                        {
+                            literal = "MA";
+                        }
+                        else if ( ltr_id % 5 == 1 )
+                        {
+                            literal = "MI";
+                        }
+                        else if ( ltr_id % 5 == 2 )
+                        {
+                            literal = "MU";
+                        }
+                        else if ( ltr_id % 5 == 3 )
+                        {
+                            literal = "ME";
+                        }
+                        else if ( ltr_id % 5 == 4 )
+                        {
+                            literal = "MO";
+                        }
+                    }
+                    else if ( ltr_id < 40 )
+                    {
+                        if ( ltr_id % 5 == 0 )
+                        {
+                            literal = "RA";
+                        }
+                        else if ( ltr_id % 5 == 1 )
+                        {
+                            literal = "RI";
+                        }
+                        else if ( ltr_id % 5 == 2 )
+                        {
+                            literal = "RU";
+                        }
+                        else if ( ltr_id % 5 == 3 )
+                        {
+                            literal = "RE";
+                        }
+                        else if ( ltr_id % 5 == 4 )
+                        {
+                            literal = "RO";
                         }
                     }
                     else
                     {
-                        if ( ltr_id == 40 || ltr_id == 43 )
+                        if ( ltr_id == 40 )
                         {
-                            literal += "a";
+                            literal = "YA";
                         }
                         else if ( ltr_id == 41 )
                         {
-                            literal += "u";
+                            literal = "YU";
                         }
                         else if ( ltr_id == 42 )
                         {
-                            literal += "o";
+                            literal = "YO";
+                        }
+                        else if ( ltr_id == 43 )
+                        {
+                            literal = "WA";
                         }
                         else if (ltr_id == 44 )
                         {
-                            literal += "nn";
+                            literal = "WNN";
                         }
 
                     }
@@ -118,8 +247,13 @@ namespace Marshall_Mahjong
             }
         }
 
-        public void generatePile ( int[] pile )
+        public void generatePile ( ref int[] pile )
         {
+            for (int i=0; i<136; i++)
+            {
+                pile[i] = i;
+            }
+            
             if ( pile.Length == 136 )
             {
                 int tmp, r;
--- Assets/Scripts/Marshall_Mahjong_Test.cs
+++ Assets/Scripts/Marshall_Mahjong_Test.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using Marshall_Mahjong;
 using System;
+using Microsoft.Unity.VisualStudio.Editor;
 
 public class Marshall_Mahjong_Test : MonoBehaviour
 {
@@ -10,8 +12,93 @@ public class Marshall_Mahjong_Test : MonoBehaviour
 
     Hai hai = new Marshall_Mahjong.Hai();
 
+    private GameObject[] river = new GameObject[18];
+    private GameObject[] dora = new GameObject[2];
+    private GameObject[] hand = new GameObject[13];
+    private GameObject tumo;
+
+    private int tumo_index;
+
     void Start()
     {
-        hai.generatePile(pile);
+        initGame();
+    }
+
+    void initGame(){
+        hai.generatePile( ref pile );
+
+        tumo = GameObject.Find( "Tumo" );
+
+        for ( int i=0; i<river.Length; i++ )
+        {
+            river[i] = GameObject.Find( "Tile" + i );
+        }
+
+        for ( int i = 0; i < hand.Length; i++ )
+        {
+            hand[i] = GameObject.Find( "Hand" + i );
+        }
+
+        for ( int i = 0; i < dora.Length; i++ )
+        {
+            dora[i] = GameObject.Find( "Dora" + i );
+        }
+
+        for ( int i = 0; i < river.Length; i++ )
+        {
+            river[i].GetComponent<SpriteRenderer>().sprite = null;
+        }
+
+        for (int i=0; i<13; i++){
+            if ( i >= hand.Length ) Debug.Log( "wrong range hand array" );
+            SpriteRenderer tmp_ = hand[i].GetComponent<SpriteRenderer>();
+
+            Addressables.LoadAssetAsync<Sprite>( hai.id2Str(pile[i]) ).Completed += handle => 
+            {
+                if ( handle.Result == null ) 
+                {
+                    Debug.Log( "Load Error" );
+                    return;
+                }
+                tmp_.sprite = handle.Result;
+            };
+        }
+
+        {
+            SpriteRenderer tmp_ = dora[0].GetComponent<SpriteRenderer>();
+            dora[0].GetComponent<SpriteRenderer>().sprite = null;
+
+            Addressables.LoadAssetAsync<Sprite>( hai.id2Str(pile[130]) ).Completed += handle => 
+            {
+                if ( handle.Result == null ) 
+                {
+                    Debug.Log( "Load Error" );
+                    return;
+                }
+                tmp_.sprite = handle.Result;
+            };
+        }
+
+        {
+            tumo_index = 13;
+            Tumo();
+        }
+    }
+
+    void Tumo()
+    {
+        SpriteRenderer tmp_ = tumo.GetComponent<SpriteRenderer>();
+
+        Addressables.LoadAssetAsync<Sprite>( hai.id2Str( pile[tumo_index]) ).Completed += handle => 
+        {
+            if ( handle.Result == null ) 
+            {
+                Debug.Log( "Load Error" );
+                return;
+            }
+            tmp_.sprite = handle.Result;
+        };
+
+        tumo_index++;
     }
 }
